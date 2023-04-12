2762c1faab3e649608aa3d893a7baf2a86b91f48
ShecktorS <ettoresanfilippo@gmail.com>
2023-04-12 17:30:17 +0200

    Header - sistemazione iniziale senza miniModal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import styles from '../scss/pages/index.module.scss';
-import Navbar from '../components/navbar/Navbar';
+import styles from "../scss/pages/index.module.scss";
+import Navbar from "../components/navbar/Navbar";
+
+import Header from "../components/header";
 
 const Home = () => {
   return (
     <section
       className={`${styles.Home} section flex flex-column justify-content-center align-items-center`}
     >
+      <Header username="Pippo" />
       <Navbar />
       <h1>Home Page</h1>
     </section>
