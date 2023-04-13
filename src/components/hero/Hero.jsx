6ed0db6697aa7be10981161b3b2904ebdd936bfc
Andrews46 <andrea4691ct@gmail.com>
2023-04-13 11:27:30 +0200

    feat:Hero

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,24 @@
+import styles from "./index.module.scss";
+
+const Hero = () => {
+  const image = [
+    "https://www.themoviedb.org/t/p/original/dBkvawTlvciUvW7jmNAtuvhdtdr.jpg",
+    " https://www.themoviedb.org/t/p/original/5i6SjyDbDWqyun8klUuCxrlFbyw.jpg",
+    "https://www.themoviedb.org/t/p/original/evaFLqtswezLosllRZtJNMiO1UR.jpg",
+  ];
+
+  return (
+    <div className={styles.Hero}>
+      <div className={styles.imageContainer}>
+        {image.map((image, index) => {
+          return (
+            <div className={styles.singleImage}>
+              <img key={index} src={image} alt={index} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
-import styles from '../scss/pages/index.module.scss';
-import Navbar from '../components/navbar/Navbar';
-
+import styles from "../scss/pages/index.module.scss";
+import Navbar from "../components/navbar/Navbar";
+import Hero from "../components/hero";
 const Home = () => {
   return (
     <section
-      className={`${styles.Home} section flex flex-column justify-content-center align-items-center`}
+    // className={`${styles.Home} section flex flex-column justify-content-center align-items-center`}
     >
       <Navbar />
       <h1>Home Page</h1>
+      <Hero />
     </section>
   );
 };
