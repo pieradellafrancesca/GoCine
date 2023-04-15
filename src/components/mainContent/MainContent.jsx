c6d6ef4520c1a1abfafc1bf65c4d09f9b5c28305
Clod91 <claudio.loreto91@gmail.com>
2023-04-15 08:57:53 +0200

    feat:fixHome

--- src/components/mainContent/MainContent.jsx
+++ src/components/mainContent/MainContent.jsx
@@ -1,11 +1,9 @@
 import styles from "./index.module.scss";
 import CardList from "../cardList";
-import Categories from "../categories";
 
 export default function MainContent() {
   return (
     <div className={styles.MainContent}>
-      <Categories />
       <CardList endpoint={"now_playing"}>
         <h2 className={styles.catTitle}>now playing</h2>
       </CardList>
