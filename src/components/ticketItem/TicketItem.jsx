d7cb9770419e284cd234a57d5dcc716ff86722b0
ShecktorS <ettoresanfilippo@gmail.com>
2023-05-04 21:01:37 +0200

    Feat: TicketToPrint added and css opt

--- src/components/ticketItem/TicketItem.jsx
+++ src/components/ticketItem/TicketItem.jsx
@@ -1,25 +1,18 @@
 import styles from "./index.module.scss";
-import { useState } from "react";
 
-const TicketItem = ({ ticket }) => {
-  const [ticketLineIsActive, setTicketLineIsActive] = useState(false);
-  const onHandleClickTicket = () => setTicketLineIsActive((prev) => !prev);
-
+const TicketItem = ({ ticket, setTicketContext }) => {
+  const onHandleClickTicket = () =>
+    setTicketContext((prev) => ({
+      isVisible: !prev.isVisible,
+      payload: { ...ticket },
+    }));
   return (
-    <>
-      <div onClick={onHandleClickTicket} className={styles.TicketItem}>
-        <h4>
-          {new Date(+ticket.date).toLocaleTimeString().slice(0, 5)} /{" "}
-          {"Poltrona n." + ticket.seatNum + 1}
-        </h4>
-      </div>
-
-      <div
-        className={`${styles.seeTicket} ${ticketLineIsActive && styles.show}`}
-      >
-        <p>Vedi il ticket</p>
-      </div>
-    </>
+    <div onClick={onHandleClickTicket} className={styles.TicketItem}>
+      <h4>
+        {new Date(+ticket.date).toLocaleTimeString().slice(0, 5)} • Poltrona n.{" "}
+        <span>{ticket.seatNum + 1}</span>
+      </h4>
+    </div>
   );
 };
 
